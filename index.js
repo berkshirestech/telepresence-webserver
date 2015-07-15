6461df3bd42dc26670f56b52485e5d487ef8af06
Russell Leggett <russ@taggartsoftware.com>
2015-07-14 20:21:52 -0400

    Updated to allow sending down commands for the head

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:'+process.env.PORT || 3000);
+  console.log('listening on *:'+(process.env.PORT || 3000));
 });
